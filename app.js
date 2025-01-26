94b575a8151d071cf8523832e2af16dfe0f7db69
Felipe Chacon <andres.chacon@powerdigitalmarketinginc.com>
2025-01-25 23:07:56 -0500

    feat: web chat static page

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const WebSocket = require('ws');
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+    if (req.method === 'GET' && req.url === '/') {
+        // Lee el archivo index.html y lo envía como respuesta
+        fs.readFile(path.join(__dirname, 'index.html'), 'utf8', (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Error al leer el archivo');
+                return;
+            }
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Página no encontrada');
+    }
+});
+
 const wss = new WebSocket.Server({ server });
 
 const clients = new Set();
